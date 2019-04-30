503ea301fca6bc6e65a4e81968f4558b2b3fadfb
LiberaLongo <liberalongo@gmail.com>
2019-04-30 14:28:52 +0200

    implementate ausiliarie

--- utils/Lista.cpp
+++ utils/Lista.cpp
@@ -22,12 +22,12 @@ virtual Lista<Tipo>::~Lista(void)
 
 //setters
 template <typename Tipo>
-void Lista<Tipo>::setHead(struct Elem* head)
+void Lista<Tipo>::setHead(struct Elem *head)
 {
     this.head = head;
 }
 template <typename Tipo>
-struct Elem* Lista<Tipo>::getHead(void)
+struct Elem *Lista<Tipo>::getHead(void)
 {
     return this.head;
 }
@@ -38,44 +38,44 @@ bool Lista<Tipo>::empty(void)
     return (this.head->next == this.head->prev == this.head);
 }
 template <typename Tipo>
-struct Elem* Lista<Tipo>::head(void)
+struct Elem *Lista<Tipo>::head(void)
 {
     return this.head->next;
 }
 template <typename Tipo>
-struct Elem* Lista<Tipo>::tail(void)
+struct Elem *Lista<Tipo>::tail(void)
 {
     return this.head->prev;
 }
 template <typename Tipo>
-struct Elem* Lista<Tipo>::next(struct Elem* p)
+struct Elem *Lista<Tipo>::next(struct Elem *p)
 {
     return p->next;
 }
 template <typename Tipo>
-struct Elem* Lista<Tipo>::prev(struct Elem* p)
+struct Elem *Lista<Tipo>::prev(struct Elem *p)
 {
     return p->prev;
 }
 template <typename Tipo>
-bool Lista<Tipo>::finished(struct Elem* p)
+bool Lista<Tipo>::finished(struct Elem *p)
 {
     return (p == this.head);
 }
 template <typename Tipo>
-Tipo Lista<Tipo>::read(struct Elem* p)
+Tipo Lista<Tipo>::read(struct Elem *p)
 {
     return p->value;
 }
 template <typename Tipo>
-struct Elem* Lista<Tipo>::write(struct Elem* p, Tipo v)
+struct Elem *Lista<Tipo>::write(struct Elem *p, Tipo v)
 {
     p->value = v;
 }
 template <typename Tipo>
-struct Elem* Lista<Tipo>::insert(struct Elem* p, Tipo v)
+struct Elem *Lista<Tipo>::insert(struct Elem *p, Tipo v)
 {
-    struct Elem* inserito;
+    struct Elem *inserito;
     inserito->value = v;
     inserito->prev = p->prev;
     inserito->prev->next = inserito;
@@ -84,11 +84,11 @@ struct Elem* Lista<Tipo>::insert(struct Elem* p, Tipo v)
     return inserito;
 }
 template <typename Tipo>
-struct Elem* Lista<Tipo>::remove(struct Elem* p)
+struct Elem *Lista<Tipo>::remove(struct Elem *p)
 {
     p->prev->next = p->next;
     p->next->prev = p->prev;
-    struct Elem* temp = p->next;
+    struct Elem *temp = p->next;
     delete p;
     return temp;
 }
@@ -97,39 +97,58 @@ struct Elem* Lista<Tipo>::remove(struct Elem* p)
 
 //cerca v scorrendo la lista
 template <typename Tipo>
-struct Elem* Lista<Tipo>::search(Tipo v) {
-
+struct Elem *Lista<Tipo>::search(Tipo v)
+{
+    struct Elem *iter = this.head();
+    bool trovato = false;
+    struct Elem *find = NULL;
+    //se non vuota e non finita, oppure lo hai trovato
+    while (!(this.empty() && this.finished(iter)) || trovato)
+    {
+        //se lo trovi
+        if (this.read(iter) == v)
+        {
+            find = iter;
+            trovato = true;
+        }
+        iter = this.next(iter);
+    }
+    return find;
 }
 //inserisci in testa
 template <typename Tipo>
-void Lista<Tipo>::insert_head(Tipo v) {
-
+void Lista<Tipo>::insert_head(Tipo v)
+{
+    this.insert(this.head(), v);
 }
 //inserisci in coda
 template <typename Tipo>
-void Lista<Tipo>::insert_tail(Tipo v){
-
+void Lista<Tipo>::insert_tail(Tipo v)
+{
+    this.insert(this.tail(), v);
 }
 //rimuovi in testa
 template <typename Tipo>
-struct Elem* Lista<Tipo>::remove_head(void){
-
+struct Elem *Lista<Tipo>::remove_head(void)
+{
+    this.remove(this.head());
 }
 //rimuovi in coda
 template <typename Tipo>
-struct Elem* Lista<Tipo>::remove_tail(void){
-    
+struct Elem *Lista<Tipo>::remove_tail(void)
+{
+    this.remove(this.tail());
 }
 
 //stampe
-void printElem(struct Elem* p)
+void printElem(struct Elem *p)
 {
-    cout << (char) this.read(p);
+    cout << (char)this.read(p);
 }
 void printList(void)
 {
     //primo elemento utile non la sentinella
-    struct Elem* iter = this.head();
+    struct Elem *iter = this.head();
     //se non vuota e non finita
     while (!(this.empty() && this.finished(iter)))
     {
