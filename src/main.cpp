1e94911a44b7382000180c0f2acc2777bdeccb78
LiberaLongo <liberalongo@gmail.com>
2019-04-30 14:55:54 +0200

    in fase di correzione

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 
-#include "./header/Gioco.hpp"
-#include "./header/Lista.hpp"
+#include "../header/Gioco.hpp"
+#include "../utils/Lista.hpp"
 
 using namespace std;
 
 int main()
 {
   //testo cose
-  List<int> lista = new List<int>();
+  Lista<int> lista();
+  struct Elem* head = lista.head();
   lista.insert_head(1);
   lista.insert_head(2);
   lista.insert_head(3);
   lista.printList();
-  lista = lista.remove(search(2));
+  head = lista.remove(lista.search(2));
   lista.printList();
   //chiamo funzione gioco
   //errore è true se si è presentato un errore, false altrimenti
