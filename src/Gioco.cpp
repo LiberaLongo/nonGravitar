4c50831e99fc2681957fd8dd1e779e7a43c93211
LiberaLongo <liberalongo@gmail.com>
2019-05-07 14:03:54 +0200

    gioco: visuale lobby iniziata

--- src/Gioco.cpp
+++ src/Gioco.cpp
@@ -1,6 +1,40 @@
 //codice gioco
-bool Gioco(void) {
+#include <SFML/Graphics.hpp>
+
+bool Gioco(void)
+{
     //codice della gestione del gioco
-    return true;
+
+    //funzione finestra di SFML
+    double width = 400.0, height = 500.0;
+    double raggio = width / 3;
+    double widthTronco = width / 4;
+    // create the lobby
+    sf::RenderWindow lobby(sf::VideoMode(width, height), "My lobby");
+
+    // run the program as long as the window is open
+    while (lobby.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (lobby.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                lobby.close();
+        }
+
+        // clear the window with black color
+        lobby.clear(sf::Color::Blue);
+
+        // draw everything here...
+
+        // lobby.draw(...);
+
+        // end the current frame
+        lobby.display();
+    }
+
+    return false;
     //ritorno VERO perchè il gioco non è stato ancora implementato
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
 
-//#include "../header/Gioco.hpp"
+//#include <SFML/Graphics.hpp>
+
+#include "../header/Gioco.hpp"
 #include "../header/Lista.hpp"
 #include "../header/Punto.hpp"
 
@@ -13,6 +15,7 @@ using namespace std;
 
 int main()
 {
+	/*
 	float X[] = {1, 2, 3, 4, 5};
 	float Y[] = {5, 4, 3, 2, 1};
 	//testo cose
@@ -24,8 +27,9 @@ int main()
 		lista.insert_head(p);
 	}
 	lista.print();
+	*/
 	//chiamo funzione gioco
 	//errore è true se si è presentato un errore, false altrimenti
-	//bool errore = Gioco();
-	//return (int) errore;
+	bool errore = Gioco();
+	return (int) errore;
 }
