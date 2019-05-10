951cc99425d5392a62223f85bdb5fb1a97dbf82a
LiberaLongo <liberalongo@gmail.com>
2019-05-10 18:22:05 +0200

    pianeta iniziato

--- header/Pianeta.hpp
+++ header/Pianeta.hpp
@@ -1,10 +1,36 @@
 //header pianeta
-#include "../utils/Lista.hpp"
-#include "../utils/Punto.hpp"
+#ifndef PIANETA_H
+#define PIANETA_H
 
-class Planet
+#include "../header/ListaClasse.hpp"
+#include "../header/Punto.hpp"
+
+class Pianeta
 {
 protected:
-    int circumference;
-    Lista<Punto>
-}
+    Punto centro;
+    float circonferenza = 50.f;
+    ListaClasse<Punto> surface;
+    //ListaClasse<Fuel> fuel;
+    //ListaClasse<Bunker> bunker;
+public:
+    //COSTRUTTORI
+    //costruttore void
+    Pianeta(void);
+    //costruttori punto
+    Pianeta(Punto centro);
+    Pianeta(float x, float y);
+    //costruttori completi
+    Pianeta(Punto centro, float circonferenza, ListaClasse<Punto> headSurface/* ListaClasse<Fuel> headFuel, ListaClasse<Bunker> headBunker*/);
+    Pianeta(float x, float y, float circonferenza, ListaClasse<Punto> headSurface/* ListaClasse<Fuel> headFuel, ListaClasse<Bunker> headBunker*/);
+    //distruttori
+    //SETTERS
+    //getters
+    //stampa
+    //disegna
+    //disegna(void);
+    //altre cose
+    //generaPianeta(void);
+
+};
+#endif
