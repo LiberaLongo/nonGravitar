89da7bfc5378d910bf27ddd39f1e98b3306c5356
LiberaLongo <liberalongo@gmail.com>
2019-06-19 11:22:50 +0200

    aggiustati errori nel modifica di parametri

--- main/Gioco.cpp
+++ main/Gioco.cpp
@@ -6,20 +6,17 @@
 
 //condizioni di esistenza per evitare bug eccessivi nel gioco
 #define MIN_SCHERMO 50.f
-#define MAX_SIZE_NAVICELLA 20.f
-#define MIN_SPEED_NAVICELLA 1.f
-#define MAX_SPEED_NAVICELLA 20.f
+#define MAX_NAVICELLA 20.f
+
 
 //dimensioni di DEFAULT
-//dimenzioni dello SCHERMO
 float WIDTH = 900.f;
 float HEIGHT = 600.f;
-//dimensioni della navicella
-float SIZE_NAVICELLA = 10.f;
-float SPEED_NAVICELLA = 5.f;
 //raggio minimo e massimo del pianeta
 float MIN_PIANETA_RAGGIO = 20.f;
 float MAX_PIANETA_RAGGIO = 60.f;
+//dimensioni della navicella
+float SIZE_NAVICELLA = 10.f;
 
 SistemaSolare sistemasolare;
 //puntatore alla posizione del pianeta corrente
@@ -27,6 +24,81 @@ struct Elem<Pianeta> *pianetaInsideNow = nullptr;
 
 void impostazioniGioco(void)
 {
+	char chose;
+	bool errato = true;
+
+	cout << "\nVuoi MODIFICARE i settaggi di default?";
+	cout << "\n(Y/N o un altro tasto per continuare)\t";
+	//chiedo se si vogliono usare le impostazioni di default
+	cin >> chose;
+
+	//se si vogliono cambiare gestisco ciò
+	if (chose == 'y' || chose == 'Y')
+	{
+		cout << "\nHai deciso di MODIFICARE le impostazioni.\n";
+		cout << "\nPuoi DECIDERE:";
+
+		//dimensioni SCHERMO
+		do
+		{
+			cout << "\nle dimensioni dello SCHERMO\n";
+			cout << "\tLargezza (>= " << MIN_SCHERMO << ", default = " << WIDTH << " ):\t";
+			cin >> WIDTH;
+			cout << "\tAltezza (>= " << MIN_SCHERMO << ", default = " << HEIGHT << "):\t\t";
+			cin >> HEIGHT;
+			//controllo se c'è un errore
+			errato = (WIDTH < MIN_SCHERMO || HEIGHT < MIN_SCHERMO);
+			//non ho modo di controllare se sono maggiori dello schermo del pc
+			//almeno per ora
+			if (errato)
+			{
+				cout << "\nERRORE! Inserisci delle dimensioni dello schermo corrette";
+				cout <<"\n(comprese tra " << MIN_SCHERMO << " e la dimensione del tuo fullscreen)";
+			}
+		} while (errato);
+
+		//dimensioni PIANETA
+		do
+		{
+			cout << "\nle dimensioni del PIANETA\n";
+			cout << "\tminimo (default = " << MIN_PIANETA_RAGGIO << "):\t\t\t";
+			cin >> MIN_PIANETA_RAGGIO;
+			cout << "\tmassimo (default = " << MAX_PIANETA_RAGGIO << "):\t\t\t"; 
+			cin >> MAX_PIANETA_RAGGIO;
+			//controllo se c'è un errore
+			errato = (MIN_PIANETA_RAGGIO < 0 || MIN_PIANETA_RAGGIO < 0 || MAX_PIANETA_RAGGIO < MIN_PIANETA_RAGGIO);
+			//non ho modo di controllare se sono maggiori dello schermo del pc
+			//almeno per ora
+			if (errato)
+			{
+				cout << "\nERRORE! Inserisci delle dimensioni del pianeta corrette";
+				cout << "\n(maggiori di 0, e con minimo < massimo)";
+			}
+		} while (errato);
+
+		//dimensioni NAVICELLA
+		do
+		{
+			cout << "\nla dimensione della NAVICELLA\n";
+			cout << "\tsize (<= " << MAX_NAVICELLA << ", default = " << SIZE_NAVICELLA << "):\t\t";
+			cin >> SIZE_NAVICELLA;
+			//controllo se c'è un errore
+			errato = (SIZE_NAVICELLA < 0 || SIZE_NAVICELLA > MAX_NAVICELLA);
+			//non ho modo di controllare se sono maggiori dello schermo del pc
+			//almeno per ora
+			if (errato)
+			{
+				cout << "\nERRORE! Inserisci delle dimensioni della navicella corrette";
+				cout << "\n(comprese tra 0 e " << MAX_NAVICELLA << ")";
+			}
+		} while (errato);
+	}
+	else
+	{
+		cout << "\nHai deciso di TENERE le PRE-impostazioni di DEFAULT.\n";
+	}
+
+	cout << "\nINIZIA IL GIOCO!\n";
 }
 
 int Gioco(void)
